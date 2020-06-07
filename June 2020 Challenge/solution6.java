28fabc8bd804f29379efb961ec7c1c8efa0abce3
Harman Preet Singh <singh.harm@husky.neu.edu>
2020-06-07 00:16:22 -0400

    June 2020 Challenge Day 6

--- June 2020 Challenge/solution6.java
+++ June 2020 Challenge/solution6.java
@@ -1,5 +1,16 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
+
+    	// sort in descending accord to the height, if height is same, ascending order of k
+        Arrays.sort(people, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0]);
         
+        int n = people.length;
+        List<int[]> res = new ArrayList();
+        
+        for(int i = 0; i < n; i++){
+            res.add(people[i][1], people[i]); //greedy approach
+        }
+        
+        return res.toArray(new int[n][2]);    
     }
 }
