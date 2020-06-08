b48d2887a98e364d3e03ddf3a8ce8db9ae7e026d
harman <harman@Amandeeps-MacBook-Pro-2.local>
2020-06-08 16:43:31 -0400

    June Challenge 2020 Day 7

--- June 2020 Challenge/solution7.java
+++ June 2020 Challenge/solution7.java
@@ -1,5 +1,13 @@
 class Solution {
-    public int change(int amount, int[] coins) {
+    public int change(int amount, int[] coins) { 
+        int[] cache = new int[amount+1];
+        cache[0] = 1;
         
+        for(int c: coins){
+            for(int j = c; j <= amount; j++){
+                cache[j] += cache[j-c];
+            }
+        }
+        return cache[amount];
     }
 }
