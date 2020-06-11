15d9ddb9b00e43d1f9235df1ee2b37aa00749d3f
harmanpreets1ngh <singh.harm@husky.neu.edu>
2020-06-11 17:56:41 -0400

    June 2020 Challenge Day 11

--- June 2020 Challenge/solution11.java
+++ June 2020 Challenge/solution11.java
@@ -1,5 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
+      
+        int left = 0, right = nums.length - 1;
+        int curr = 0;
         
+        while(curr <= right){
+            if(nums[curr] == 0){
+                nums[curr] = nums[left];
+                nums[left] = 0; //swapping with 0
+                curr++;
+                left++;
+            }else if(nums[curr] == 2){
+                nums[curr] = nums[right];
+                nums[right] = 2; //swapping with 2
+                right--;
+            }else{
+                curr++;
+            }
+        }
     }
 }
