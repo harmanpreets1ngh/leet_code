e9ab5d42ad381c07c092a69ef493b600622f7a83
harmanpreets1ngh <singh.harm@husky.neu.edu>
2020-06-15 04:43:39 -0400

    June 2020 Challenge Day 13

--- June 2020 Challenge/solution13.java
+++ June 2020 Challenge/solution13.java
@@ -1,5 +1,32 @@
 class Solution {
-    public List<Integer> largestDivisibleSubset(int[] nums) {
+	public List<Integer> largestDivisibleSubset(int[] nums) {
         
+        List<Integer> res = new ArrayList();
+         if (nums == null || nums.length == 0) {
+            return res;
+        }
+        
+        Arrays.sort(nums);
+        List<List<Integer>> cache = new ArrayList();
+        for(int num: nums) { cache.add(new ArrayList()); }
+        
+        for(int i = 0; i < nums.length; i++){
+            List<Integer> newSet = new ArrayList();
+            
+            for(int j = 0; j < i; j++){
+                if(nums[i] % nums[j] == 0 && newSet.size() < cache.get(j).size()){
+                    newSet = cache.get(j);
+                }
+            }    
+            cache.get(i).add(nums[i]);
+            cache.get(i).addAll(newSet);      
+        }
+        
+        for(int i = 0; i < cache.size(); i++){
+            if(res.size() < cache.get(i).size()){
+                res = cache.get(i);
+            }
+        }  
+        return res;
     }
 }
